cb25b45a407021bbec6e6db218958b9f72acc3e4
Farhan0021 <mdfarhanahmed2100@gmail.com>
2025-07-25 23:52:29 +0530

    allroute updaet

--- front/src/routes/AllRoutes.jsx
+++ front/src/routes/AllRoutes.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import {
-    Home,
-SeekerLogin,
-SeekerSignup,
-OwnerLogin,
-OwnerSignup,
-Details,
-SeekerLogout,
-OwnerLogout
+  Home,
+  SeekerLogin,
+  SeekerSignup,
+  OwnerLogin,
+  OwnerSignup,
+  Details,
+  SeekerLogout,
+  OwnerLogout
 } from '../pages'
 import Myprofile from '../pages/seekers/MyProfile'
 import SeekerProtactedRoutes from './SeekerProtactedRoutes'
@@ -22,46 +22,40 @@ import UpdatePass from '../pages/seekers/UpdatePass'
 import ForgotPassword from '../pages/seekers/ForgotPassword'
 import Otp from '../pages/seekers/Otp'
 import ChangePassword from '../pages/seekers/ChangePassword'
+
 const AllRoutes = () => {
-    return(
-        <>
-        <Routes>
-            <Route path='' element= { <Home/>}/>
-            <Route path='details/:id' element={<Details/>}/>
-            <Route path='seeker/login' element={<SeekerLogin/>}/>
-            <Route path='seeker/forgot-password' element={<ForgotPassword/>}/>
-            <Route path='seeker/otp' element={<Otp />} />
-            <Route path='seeker/change-password' element={<ChangePassword />} />
-            <Route path='' element={<SeekerProtactedRoutes/>}>
+  return (
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<Home />} />
+      <Route path="details/:id" element={<Details />} />
+      <Route path="seeker/login" element={<SeekerLogin />} />
+      <Route path="seeker/signup" element={<SeekerSignup />} />
+      <Route path="seeker/forgot-password" element={<ForgotPassword />} />
+      <Route path="seeker/otp" element={<Otp />} />
+      <Route path="seeker/change-password" element={<ChangePassword />} />
 
-            <Route path='seeker/my-profile' element={<Myprofile/>}/>
-            <Route path='seeker/update-password' element={<UpdatePass/>}/>
-            <Route path='seeker/edit-profile' element={<EditProfile/>}/>
-            <Route path='seeker/logout' element={<SeekerLogout/>}/>
+      <Route path="owner/login" element={<OwnerLogin />} />
+      <Route path="owner/signup" element={<OwnerSignup />} />
 
-            </Route>
+      {/* Seeker Protected Routes */}
+      <Route path="seeker" element={<SeekerProtactedRoutes />}>
+        <Route path="my-profile" element={<Myprofile />} />
+        <Route path="edit-profile" element={<EditProfile />} />
+        <Route path="update-password" element={<UpdatePass />} />
+        <Route path="logout" element={<SeekerLogout />} />
+      </Route>
 
-            <Route path='seeker/Signup' element={<SeekerSignup/>}/>
-
-            <Route path='owner/Login' element={<OwnerLogin/>}/>
-            <Route path='owner/Signup' element={<OwnerSignup/>}/>
-
-            <Route path='' element={<OwnerProtactedRoutes/>}>
-
-            <Route path='owner/add-new-property' element={<AddNew/>}/>
-            <Route path='owner/my-property' element={<MyProperty/>}/>
-            
-            <Route path='owner/my-account' element={<MyAccount/>}/>
-            <Route path='owner/logout' element={<OwnerLogout/>}/>
-            <Route path='owner/messages/:id' element={<Messages />} />
-
-            </Route>
-            
-
-        </Routes>
-
-     </>
-
-    )
+      {/* Owner Protected Routes */}
+      <Route path="owner" element={<OwnerProtactedRoutes />}>
+        <Route path="add-new-property" element={<AddNew />} />
+        <Route path="my-property" element={<MyProperty />} />
+        <Route path="my-account" element={<MyAccount />} />
+        <Route path="logout" element={<OwnerLogout />} />
+        <Route path="messages/:id" element={<Messages />} />
+      </Route>
+    </Routes>
+  )
 }
-export default AllRoutes 
+
+export default AllRoutes
